6b6308f4c7a9a9852df1b165c4291547414802b2
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-06-17 12:07:12 +0530

    Update App.jsx

--- React-States/src/App.jsx
+++ React-States/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
       <h1>States in React</h1>
-      <Counter />
-      <button onClick={incCount}>Increase Counter</button>
+      {/* <Counter /> */}
+      <button>Increase Counter</button>
     </>
   );
 }
