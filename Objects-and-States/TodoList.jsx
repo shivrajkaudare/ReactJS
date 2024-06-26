96ea4e527157facea0a6cf3ba8fa99e4dcafe125
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-06-26 13:56:36 +0530

    Update TodoList.jsx

--- Objects-and-States/TodoList.jsx
+++ Objects-and-States/TodoList.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 export default function TodoList() {
   let Style = {
-    height: 33,
-    width: 250,
-    borderRadius: 10,
+    height: "33px",
+    width: "250px",
+    borderRadius: "10px",
+    color: "white",
   };
+
+  let [todos, setTodo] = useState([{ task: "sample task", id: uuidv4() }]);
+  let [newTodo, setnewTodo] = useState("");
+
+  let addNewTask = () => {
+    setTodo([...todos, newTodo]);
+    setnewTodo("");
+  };
+
+  let updateTodoValue = (event) => {
+    setnewTodo(event.target.value);
+  };
+
   return (
     <div>
-      <input placeholder="add a task" style={Style}></input>
+      <input
+        placeholder="add a task"
+        style={Style}
+        value={newTodo}
+        onChange={updateTodoValue}
+      ></input>
       <br></br>
       <br></br>
-      <button>Add Task</button>
+      <button onClick={addNewTask}>Add Task</button>
       <br />
       <br />
       <br />
@@ -17,7 +39,11 @@ export default function TodoList() {
       <br />
       <hr></hr>
       <h3>Tasks to do</h3>
-      <ul></ul>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
     </div>
   );
 }
