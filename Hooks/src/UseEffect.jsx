dcbf2471d6f77037180b1bc52688ba2cb383cae2
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-07-02 20:19:29 +0530

    Update UseEffect.jsx

--- Hooks/src/UseEffect.jsx
+++ Hooks/src/UseEffect.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Counter() {
   let [count, setCount] = useState(0);
+
+  let incCount = () => {
+    setCount((currCount) => currCount + 1);
+  };
+
+  // this function execute everytime when component re-renders
+  useEffect(function printSomething() {
+    console.log("This is a side-effect");
+  });
   return (
     <div>
       <h3>count = {count}</h3>
-      <button onClick={intCount}>+1</button>
+      <button onClick={incCount}>+1</button>
     </div>
   );
 }
