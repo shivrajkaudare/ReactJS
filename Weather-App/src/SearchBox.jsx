df38dbcbd5513204ea5c00cde2ed90dfb1efcc62
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-07-06 19:06:07 +0530

    Update SearchBox.jsx

--- Weather-App/src/SearchBox.jsx
+++ Weather-App/src/SearchBox.jsx
@@ -2,17 +2,47 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./SearchBox.css";
 import { useState } from "react";
-export default function SearchBox() {
-  let [city, setCity] = useState("");
 
-  let handleChange = (event) => {
+export default function SearchBox() {
+  const [city, setCity] = useState("");
+
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "1889c3d5c76bb3e4c2363bebb42b79a3";
+
+  const getWeatherInfo = async () => {
+    try {
+      const response = await fetch(
+        `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      let result = {
+        temp: jsonResponse.main.temp,
+        tempMin: jsonResponse.main.temp_min,
+        tempMax: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        feelsLike: jsonResponse.main.feels_like,
+        weather: jsonResponse.weather[0].description,
+      };
+      console.log(result);
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  };
+
+  const handleChange = (event) => {
     setCity(event.target.value);
   };
-  let handleSubmit = (event) => {
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(city);
+    getWeatherInfo();
     setCity("");
   };
+
   return (
     <div className="SearchBox">
       <h3>Search For a Weather</h3>
