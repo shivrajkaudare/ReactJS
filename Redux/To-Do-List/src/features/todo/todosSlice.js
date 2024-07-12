f36676d3056aa968de9075f51547a80cbd410a3a
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-07-12 16:49:17 +0530

    added reducers in store.js

--- Redux/To-Do-List/src/features/todo/todosSlice.js
+++ Redux/To-Do-List/src/features/todo/todosSlice.js
@@ -10,13 +10,30 @@ export const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
+    // reducer for adding new todo.
     addTodo: (state, action) => {
       const newTodo = {
         id: nanoid(),
         task: action.payload,
         isDone: false,
       };
-      state.todos.push(newTodo);
+      state.todos.push(newTodo); // direct mutation of array but in react we have to destructure array.
+    },
+    // reducer for delete todo
+    deleteDodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    marksAsDone: (state, action) => {
+      state.todos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          todo.isDone = true;
+        }
+      });
     },
   },
 });
+
+// equating  all reducers to let redux toolkit know for generating action creators.
+export const { addTodo, deleteDodo, marksAsDone } = todoSlice.actions;
+
+export default todoSlice.reducer;
